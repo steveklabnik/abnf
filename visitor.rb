cb0e887186d97c539ae9e364204fb83bdc71964e
Tanaka Akira <akr@fsij.org>
2002-01-21 09:12:23 +0000

    more document.

--- visitor.rb
+++ visitor.rb
@@ -4,20 +4,46 @@ visitor.rb generates stub of visitor pattern.
 Using visitor.rb, you don't need to define `accept' method for each class.
 
 == Example
-C = visitor_pattern
+  C = visitor_pattern
 
-class D < C
-end
+  class D < C
+  end
 
-class E < C
-end
+  class E < C
+  end
 
-class V < C::Visitor
-  def visit_D(d) p d end
-end
+  class V < C::Visitor
+    def visit_D(d) p d end
+  end
 
-D.new.accept(V.new) #=> #<D:0x8176048>
-E.new.accept(V.new) #=> NotImplementedError (*not* NoMethodError)
+  D.new.accept(V.new) #=> #<D:0x8176048>
+  E.new.accept(V.new) #=> NotImplementedError (*not* NoMethodError)
+
+The definitions of C, D and E are same as follows without visitor.rb.
+
+  class C
+    class Visitor
+      def visit_D(_)
+        raise NotImplementedError.new
+      end
+
+      def visit_E(_)
+        raise NotImplementedError.new
+      end
+    end
+  end
+
+  class D < C
+    def accept(v)
+      v.visit_D self
+    end
+  end
+
+  class E < C
+    def accept(v)
+      v.visit_E self
+    end
+  end
 =end
 
 def visitor_pattern(&generate_methodname)
