229d1df48d1e311d79c2e0054706599a6c8a83cb
Tanaka Akira <akr@fsij.org>
2002-01-30 10:30:24 +0000

    add unit test.

--- abstract.rb
+++ abstract.rb
@@ -1,17 +1,63 @@
 =begin
 = abstract.rb implements abstract methods for Ruby.
 
+In general, abstract methods are not required for dynamically typed
+object oriented languages such as Ruby.
+It is because existence of a method is checked at run time.
+
+This library makes it possible to check existence of a method with unit test
+before run time.
+
+== example
+
+  require 'abstract'
+
+  class A
+    define_abstract_method :m
+  end
+
+  class B < A
+    def m
+      p :B
+    end
+  end
+
+  class C < A
+  end
+
+  if __FILE__ == $0
+    require 'runit/testcase'
+    require 'runit/cui/testrunner'
+
+    class AbstractTest < RUNIT::TestCase
+      def test_abstract
+	assert_equal([], B.abstract_methods) #=> success.
+	assert_equal([], C.abstract_methods) #=> fail.
+      end
+    end
+
+    RUNIT::CUI::TestRunner.run(AbstractTest.suite)
+  end
+
+
+== methods
 --- Module#define_abstract_method name
+    defines an abstract method named as ((|name|)).
+
+    If the method is called, AbstractMethodCallError is raised.
 
 --- Module#abstract_methods
+    returns an array which contains list of non-redefined abstract method names.
+    The method names are represented as symbols.
 
 --- Module#abstract?
-
+    returns true iff there is a non-redefined abstract method.
 =end
 
+class AbstractMethodCallError < NotImplementedError
+end
+
 class Module
-  class AbstractMethodCallError < NotImplementedError
-  end
 
   AbstractMethods = {}
 
@@ -36,6 +82,65 @@ class Module
   def abstract?
     !abstract_methods.empty?
   end
+end
+
+if __FILE__ == $0
+  require 'runit/testcase'
+  require 'runit/cui/testrunner'
+
+  class AbstractTest < RUNIT::TestCase
+    def test_class
+      eval <<-End
+        class A
+	  define_abstract_method :m
+	end
+	assert_equal([:m], A.abstract_methods)
+
+	class B < A
+	end
+	assert_equal([:m], B.abstract_methods)
+
+	class C < B
+	  def m
+	  end
+	end
+	assert_equal([], C.abstract_methods)
+      End
+    end
+
+    def test_module
+      eval <<-End
+        module M
+	  define_abstract_method :m
+	end
+	assert_equal([:m], M.abstract_methods)
+
+	module N
+	  include M
+	end
+	assert_equal([:m], N.abstract_methods)
+
+	module O
+	  include N
+	  def m
+	  end
+	end
+	assert_equal([], O.abstract_methods)
+
+	class X
+	  include N
+	end
+	assert_equal([:m], X.abstract_methods)
+
+	class Y
+	  include O
+	end
+	assert_equal([], Y.abstract_methods)
+      End
+    end
+  end
+
+  RUNIT::CUI::TestRunner.run(AbstractTest.suite)
 end
 
 # Hash で集合を表す set.rb を作って使うか?
