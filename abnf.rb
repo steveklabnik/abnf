0344db767c2d940acf117d33161d55a91ebf30e1
Tanaka Akira <akr@fsij.org>
2002-10-10 10:25:52 +0000

    more simplified.

--- abnf.rb
+++ abnf.rb
@@ -47,8 +47,7 @@ class ABNF
   end
 
   def ABNF.regexp_tree(desc, name=nil)
-    grammar = ABNF.parse(desc)
-    grammar.regexp_tree(name)
+    ABNF.parse(desc).regexp_tree(name)
   end
 
   def ABNF.parse(desc, dont_merge_core_rules=false)
