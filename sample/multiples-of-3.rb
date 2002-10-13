830d1821f210784c2e1d1f30c6a3a1049f9b75fd
Tanaka Akira <akr@fsij.org>
2002-10-13 14:14:37 +0000

    use / instead of | for alternation.

--- sample/multiples-of-3.rb
+++ sample/multiples-of-3.rb
@@ -2,12 +2,12 @@ require 'abnf'
 require 'pp'
 
 rt = ABNF.regexp_tree(<<'End')
-s0 = n0 s0 | n1 s2 | n2 s1 | ""
-s1 = n0 s1 | n1 s0 | n2 s2
-s2 = n0 s2 | n1 s1 | n2 s0
-n0 = "0" | "3" | "6" | "9"
-n1 = "1" | "4" | "7"
-n2 = "2" | "5" | "8"
+s0 = n0 s0 / n1 s2 / n2 s1 / ""
+s1 = n0 s1 / n1 s0 / n2 s2
+s2 = n0 s2 / n1 s1 / n2 s0
+n0 = "0" / "3" / "6" / "9"
+n1 = "1" / "4" / "7"
+n2 = "2" / "5" / "8"
 End
 
 pp rt
